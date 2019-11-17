f35f0718293013a40da4d6e4c558cf215584b67a
Leonardo H Vieira <leohvir@gmail.com>
2019-11-17 18:40:10 -0300

    Adicionando pacode CURSES

---
+++ Jogo da velha/jogo_da_velha.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.6
+# -*- coding: UTF-8 -*-
+"""
+
+Jogo da velha
+
+Licensa:
+
+Copyright 2017 E. S. Pereira
+"""
+
+from curses import initsrc, wrapper
+from random import randint
