caf27c68931f9b5c901c704f415988d8bc4611c6
Leonardo H Vieira <leohvir@gmail.com>
2019-11-22 18:29:40 -0300

    Funções basicas jogo

--- Jogo da velha/jogo_da_velha.py
+++ Jogo da velha/jogo_da_velha.py
@@ -9,5 +9,16 @@ Licensa:
 Copyright 2017 E. S. Pereira
 """
 
-from curses import initsrc, wrapper
+from curses import initscr, wrapper
 from random import randint
+
+def main(stdscr):
+    pass
+
+if __name__ == "__main__":
+    initscr()
+    wrapper(main)
+
+def boas_vindas(stdscr):
+    stdscr.addstr(1, 1, "Bem-vindo ao Jogo da Velha")
+    stdscr.addstr(2, 1, "Pressione q para sair ou h para obter ajuda")
