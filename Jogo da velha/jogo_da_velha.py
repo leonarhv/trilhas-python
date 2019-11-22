16d3849ad6dc2d24e76671f47864acd88886d801
Leonardo H Vieira <leohvir@gmail.com>
2019-11-22 20:38:25 -0300

    funcoes basicas

--- Jogo da velha/jogo_da_velha.py
+++ Jogo da velha/jogo_da_velha.py
@@ -12,13 +12,45 @@ Copyright 2017 E. S. Pereira
 from curses import initscr, wrapper
 from random import randint
 
-def main(stdscr):
-    pass
-
-if __name__ == "__main__":
-    initscr()
-    wrapper(main)
 
 def boas_vindas(stdscr):
     stdscr.addstr(1, 1, "Bem-vindo ao Jogo da Velha")
     stdscr.addstr(2, 1, "Pressione q para sair ou h para obter ajuda")
+    stdscr.addstr(16, 1, "Desenvolvido originalmente por: E. S. Pereira.")
+    stdscr.addstr(17, 1, "Editado para estudos por: Leonardo H Vieira")
+    stdscr.addstr(18, 1, "Licensa Nova BSD.")
+
+
+def ajuda(stdscr):
+    stdscr.clear()
+    stdscr.border()
+    stdscr.addstr(1, 1, "Pressione Q para sair ou H para exibir essa janela de ajuda")
+    stdscr.addstr(2, 1, "Para mudar de posição, use as teclas: W, A, S, D")
+    stdscr.addstr(3, 1, "Para definir uma posição do jogo, digite: L")
+    stdscr.addstr(4, 1, "Para reiniciar a partida, digite: Y")
+    stdscr.addstr(5, 1, "Pressione espaço para sair dessa tela.")
+    stdscr.refresh()
+
+
+def reiniciar_tela(stdscr):
+    stdscr.clear()
+    stdscr.border()
+    boas_vindas(stdscr)
+    stdscr.refresh()
+
+
+def main(stdscr):
+    reiniciar_tela(stdscr)
+
+    while True:
+        entrada = stdscr.getkey()
+        if entrada == 'q':
+            break
+        if entrada == 'h':
+            ajuda(stdscr)
+        else:
+            boas_vindas(stdscr)
+
+if __name__ == "__main__":
+    initscr()
+    wrapper(main)
